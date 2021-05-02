6dcfb1ba890ac06f1ee2ed0a234cde9d16ab392a
0KABE <RIN.OKAB3@GMAIL.COM>
2021-05-03 02:44:01 +0800

    feat: enable Ant Design Pro Layout

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  layout: {
+    name: 'Owl',
+    layout: 'side',
+  },
+  locale: {
+    default: 'zh-CN',
+    antd: false,
+    title: false,
+    baseNavigator: true,
+    baseSeparator: '-',
+  },
   nodeModulesTransform: {
     type: 'none',
   },
