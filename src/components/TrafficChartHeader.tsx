d8ff32a96be78db38e853c3d8232b89bed2e2b57
0KABE <RIN.OKAB3@GMAIL.COM>
2021-05-04 22:22:20 +0800

    feat: update TrafficChartHeader speed unit to MB/s

--- src/components/TrafficChartHeader.tsx
+++ src/components/TrafficChartHeader.tsx
@@ -3,10 +3,10 @@ import { Statistic, Row, Col } from 'antd';
 import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
 
 export type Statistic = {
-  upload: Number;
-  download: Number;
-  uploadTotal: Number;
-  downloadTotal: Number;
+  upload: number;
+  download: number;
+  uploadTotal: number;
+  downloadTotal: number;
 };
 
 type Props = {
@@ -24,36 +24,36 @@ class TrafficChartHeader extends React.Component {
         <Col span={5}>
           <Statistic
             title="Download"
-            suffix={'KB/s'}
+            suffix={'MB/s'}
             prefix={<ArrowDownOutlined />}
-            value={this.props.statistic.download.valueOf() / 1e3}
+            value={this.props.statistic.download / 1e6}
             precision={2}
           />
         </Col>
         <Col span={5}>
           <Statistic
             title="Upload"
-            suffix={'KB/s'}
+            suffix={'MB/s'}
             prefix={<ArrowUpOutlined />}
-            value={this.props.statistic.upload.valueOf() / 1e3}
+            value={this.props.statistic.upload / 1e6}
             precision={2}
           />
         </Col>
         <Col span={5}>
           <Statistic
             title="Download total"
-            suffix={'KB/s'}
+            suffix={'MB/s'}
             prefix={<ArrowDownOutlined />}
-            value={this.props.statistic.downloadTotal.valueOf() / 1e3}
+            value={this.props.statistic.downloadTotal / 1e6}
             precision={2}
           />
         </Col>
         <Col span={5}>
           <Statistic
             title="Upload totoal"
-            suffix={'KB/s'}
+            suffix={'MB/s'}
             prefix={<ArrowUpOutlined />}
-            value={this.props.statistic.uploadTotal.valueOf() / 1e3}
+            value={this.props.statistic.uploadTotal / 1e6}
             precision={2}
           />
         </Col>
