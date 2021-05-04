b6ec59214291e1350739ce51226f081efd6c131f
0KABE <RIN.OKAB3@GMAIL.COM>
2021-05-04 22:20:42 +0800

    feat: add websocket for get current speed

--- src/components/Traffic.tsx
+++ src/components/Traffic.tsx
@@ -11,62 +11,85 @@ type Props = {
   url: URL;
 };
 
-interface Traffic {
-  props: Props;
-}
-
-class Traffic extends React.Component {
+class Traffic extends React.Component<Props> {
   statistic: Statistic = {
-    download: 111,
-    downloadTotal: 999,
-    upload: 88,
-    uploadTotal: 90.99,
+    download: 0,
+    downloadTotal: 0,
+    upload: 0,
+    uploadTotal: 0,
   };
   speeds: Speeds = { download: [], upload: [], time: [] };
 
   state: State = { statistic: this.statistic, speeds: this.speeds };
 
-  randomIntFromInterval(min: number, max: number) {
-    // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+  webSocket = new WebSocket(this.props.url.toString());
 
-  time: Date = new Date();
-  randomData(): [Number, Number, Date] {
-    this.time = new Date(this.time.getTime() + 1000);
-    return [
-      this.randomIntFromInterval(5000, 10000),
-      this.randomIntFromInterval(5000, 1000),
-      this.time,
-    ];
-  }
+  webSocketEventListener = (ev: MessageEvent<any>) => {
+    const speedData: { upload: number; download: number } = JSON.parse(ev.data);
+    console.log(speedData);
+    this.updateSpeed(speedData.download, speedData.upload);
+  };
 
-  updateData() {
-    if (this.speeds.time.length >= 10) {
-      this.speeds.download.shift();
-      this.speeds.upload.shift();
-      this.speeds.time.shift();
-    }
-    const [download, upload, time] = this.randomData();
-    this.speeds.download.push(download);
-    this.speeds.upload.push(upload);
-    this.speeds.time.push(time);
-    this.statistic.download = download;
-    this.statistic.upload = upload;
-    this.statistic.downloadTotal =
-      this.statistic.downloadTotal.valueOf() + download.valueOf();
-    this.statistic.uploadTotal =
-      this.statistic.uploadTotal.valueOf() + upload.valueOf();
+  updateSpeed(download: number, upload: number) {
+    const time = new Date();
+    this.updateSpeeds(download, upload, time);
+    this.updateStatistic(download, upload);
     this.setState({ statistic: this.statistic, speeds: this.speeds });
   }
 
-  interval!: NodeJS.Timeout;
+  updateSpeeds(download: number, upload: number, time: Date) {
+    this.speeds.download.push(download);
+    this.speeds.upload.push(upload);
+    this.speeds.time.push(time);
+  }
+
+  updateStatistic(download: number, upload: number) {
+    this.statistic.downloadTotal += download;
+    this.statistic.uploadTotal += upload;
+    this.statistic.download = download;
+    this.statistic.upload = upload;
+  }
+
+  // randomIntFromInterval(min: number, max: number) {
+  //   // min and max included
+  //   return Math.floor(Math.random() * (max - min + 1) + min);
+  // }
+
+  // time: Date = new Date();
+  // randomData(): [Number, Number, Date] {
+  //   this.time = new Date(this.time.getTime() + 1000);
+  //   return [
+  //     this.randomIntFromInterval(5000, 10000),
+  //     this.randomIntFromInterval(5000, 1000),
+  //     this.time,
+  //   ];
+  // }
+
+  // updateData() {
+  //   if (this.speeds.time.length >= 10) {
+  //     this.speeds.download.shift();
+  //     this.speeds.upload.shift();
+  //     this.speeds.time.shift();
+  //   }
+  //   const [download, upload, time] = this.randomData();
+  //   this.speeds.download.push(download);
+  //   this.speeds.upload.push(upload);
+  //   this.speeds.time.push(time);
+  //   this.statistic.download = download;
+  //   this.statistic.upload = upload;
+  //   this.statistic.downloadTotal =
+  //     this.statistic.downloadTotal.valueOf() + download.valueOf();
+  //   this.statistic.uploadTotal =
+  //     this.statistic.uploadTotal.valueOf() + upload.valueOf();
+  //   this.setState({ statistic: this.statistic, speeds: this.speeds });
+  // }
+
   componentDidMount() {
-    this.interval = setInterval(() => this.updateData(), 1000);
+    this.webSocket.addEventListener('message', this.webSocketEventListener);
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
+    this.webSocket.removeEventListener('message', this.webSocketEventListener);
   }
 
   render() {
